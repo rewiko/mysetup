dae91f0d67c3dcd1c3e131b1f24ee59e8e1a4453
Anthony Comtois <anthony.comtois@sky.uk>
2020-02-25 20:20:31 +0000

    update install

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 sudo apt-get update; sudo apt-get install -y tmux curl zsh ruby-dev git terminator vim python-pip xclip ctags ack-grep
 sudo gem install tmuxinator
-sudo pip install cheat
+#sudo pip install cheat
 wget https://github.com/cheat/cheat/releases/download/3.5.1/cheat-linux-amd64.gz
-gunzip cheat-linux-amd64.gz && sudo mv ./cheat-linux-amd64 /usr/lobal/bin/cheat
+gunzip cheat-linux-amd64.gz && sudo mv ./cheat-linux-amd64 /usr/local/bin/cheat
 sudo chmod +x /usr/local/bin/cheat
+mkdir -p ~/.config/cheat/
 mv cheat-conf.yaml ~/.config/cheat/conf.yml
 rm -rf ~/.oh-my-zsh/
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
@@ -20,6 +21,6 @@ curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
 git clone https://github.com/rewiko/myvimrc.git ~/myvimrc
 cd ~/myvimrc ; ./install.sh
 git clone https://rewiko@bitbucket.org/rewiko/cheatsheet.git ~/.cheat/personal/
-
+git  clone https://github.com/cheat/cheatsheets.git  ~/.cheat/community/
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
